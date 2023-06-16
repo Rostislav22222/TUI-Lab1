06fde70139544764082cab9e01f69303e6c3fede
Галанін Ростислав <119488812+Rostislav22222@users.noreply.github.com>
2023-06-17 00:15:42 +0300

    Add files via upload

---
+++ src/Bank.java
@@ -0,0 +1,22 @@
+package com.mybank.tui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    private List<Customer> customers;
+
+    public Bank() {
+        customers = new ArrayList<>();
+    }
+
+    public void addCustomer(Customer customer) {
+        customers.add(customer);
+    }
+
+    public Customer getCustomer(int index) {
+        if (index >= 0 && index < customers.size()) {
+            return customers.get(index);
+        }
+        return null;
+    }
